856a518b0d79ac0f8d911b3254d12ad6872975ef
Ádám Z. Kövér <adamzkover@gmail.com>
2018-03-15 10:30:36 +0100

    Set port 8081 for test of main() method

--- src/test/java/net/hunnor/dict/client/ApplicationTests.java
+++ src/test/java/net/hunnor/dict/client/ApplicationTests.java
@@ -4,9 +4,22 @@ import org.junit.Test;
 
 public class ApplicationTests {
 
+  private static final String SERVER_PORT = "server.port";
+
   @Test
   public void contextLoads() {
+
+    String port = System.getProperty(SERVER_PORT);
+    System.setProperty(SERVER_PORT, "8081");
+
     Application.main(new String[] {});
+
+    if (port == null) {
+      System.clearProperty("server.port");
+    } else {
+      System.setProperty(SERVER_PORT, port);
+    }
+
   }
 
 }
