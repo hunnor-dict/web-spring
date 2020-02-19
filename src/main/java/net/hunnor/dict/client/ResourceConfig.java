1c8a9622b99e7dd6924e53c163ee3e2bfbd3cd39
Ádám Z. Kövér <adamzkover@gmail.com>
2020-02-19 10:20:01 +0100

    Resource fingerprinting

---
+++ src/main/java/net/hunnor/dict/client/ResourceConfig.java
@@ -0,0 +1,37 @@
+package net.hunnor.dict.client;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.CssLinkResourceTransformer;
+import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
+
+@Configuration
+@EnableWebMvc
+public class ResourceConfig implements WebMvcConfigurer {
+
+  @Override
+  public final void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+    registry.addResourceHandler("//**")
+        .addResourceLocations("classpath:/public/")
+        .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS))
+        .resourceChain(true)
+        .addResolver(new VersionResourceResolver()
+            .addContentVersionStrategy("/**"))
+        .addTransformer(new CssLinkResourceTransformer());
+
+  }
+
+  @Bean
+  public ResourceUrlEncodingFilter resourceUrlEncodingFilter() {
+    return new ResourceUrlEncodingFilter();
+  }
+
+}
